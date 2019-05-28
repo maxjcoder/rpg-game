3f677c8b34eca15c51bbd8a5b6d28c4900992bf1
team awesome <maxj@email.arizona.edu>
2019-05-27 20:51:10 -0700

    click handlers for each character image

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+//Click Handlers for each chararcter image
+$(".obiImage").on("click", function() {
+    alert("you clicked on Obi Wan.");
+});
+
+$(".lukeImage").on("click", function() {
+    alert("you clicked on Luke Skywalker.");
+});
+
+$(".darSidImage").on("click", function() {
+    alert("you clicked on Darth Sidious.");
+});
+
+$(".darMaulImage").on("click", function() {
+    alert("you clicked on Darth Maul.");
+});
